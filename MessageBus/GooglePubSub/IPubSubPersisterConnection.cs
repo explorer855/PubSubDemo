4fe7bad5d9ff660e50da31164404b875cda0840e
Bikash <bikash@rx365.in>
2021-05-03 11:52:30 +0530

    committed changes

---
+++ MessageBus/GooglePubSub/IPubSubPersisterConnection.cs
@@ -0,0 +1,12 @@
+﻿//using Microsoft.Azure.ServiceBus;
+
+namespace GooglePubSub
+{
+    public interface IPubSubPersisterConnection
+    {
+        //ITopicClient TopicClient { get; }
+        //ISubscriptionClient SubscriptionClient { get; }
+
+        //ISubscriptionClient SubscriptionClientCreate(string subscriber);
+    }
+}
---
+++ MessageBus/GooglePubSub/PubSubPersisterConnection.cs
@@ -0,0 +1,19 @@
+﻿namespace GooglePubSub
+{
+    public sealed class PubSubPersisterConnection
+        : IPubSubPersisterConnection
+    {
+        bool _disposed;
+
+        public PubSubPersisterConnection()
+        {
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+
+            _disposed = true;
+        }
+    }
+}
